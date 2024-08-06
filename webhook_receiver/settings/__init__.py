09849c751fd6f10ab6385e2b3c25cd310ee9121c
yogeshbhagatcode <149584985+yogeshbhagatcode@users.noreply.github.com>
2024-08-06 14:27:30 +0530

    Changes for test

--- webhook_receiver/settings/__init__.py
+++ webhook_receiver/settings/__init__.py
@@ -169,15 +169,8 @@ if not default_db:
 # want to use them.  But if it doesn't, we still want to set OPTIONS
 # to an empty dictionary, which django-environ doesn't do by default.
 default_db.setdefault('OPTIONS', {})
-# DATABASES = {
-#     'default': default_db,
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + "/db.sqlite3",
-    }
+    'default': default_db,
 }
 
 CACHES = {
